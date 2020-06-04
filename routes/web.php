a5df7303dd33af77514235cdd17bf3f2cd8719c3
jose.vaca@decameron.com <jose.vaca@decameron.com>
2020-06-04 11:05:26 -0500

    024 Controlador CategoriaController

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('contenido/contenido');
 });
+Route::get('/categoria','Categoria\CategoriaController@index');
+Route::post('/categoria','Categoria\CategoriaController@store');
+Route::put('/categoria','Categoria\CategoriaController@update');
+Route::put('/categoria/desactivar','Categoria\CategoriaController@desactivar');
+Route::put('/categoria/activar','Categoria\CategoriaController@activar');
